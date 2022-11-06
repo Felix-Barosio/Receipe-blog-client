0c0730eb6c7d0d8be61f03d56562d281babf9331
Felix <barosiofelix@gmail.com>
2022-11-06 18:29:19 +0300

    Added home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,19 @@ import { Button } from './Button'
 function Home() {
     return (
         <div>
-
+            <div className='home-container'>
+                <h1>Receipes Blog</h1>
+                <p>What are you looking for?</p>
+                <div className='home-btns'>
+                    <Button
+                        className='btns btn btn-primary'
+                        buttonStyle='btn--outline'
+                        buttonSize='btn--large'
+                    >
+                        GET STARTED
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
