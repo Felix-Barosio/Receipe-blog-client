6ec56b5742226c1fb9adcefa7397c82df9d30386
Felix <barosiofelix@gmail.com>
2022-11-06 18:24:25 +0300

    Added nav section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,15 @@ import { Link, Outlet } from "react-router-dom"
 function NavBar() {
     return (
         <div className='header'>
-
+            <div className='navbar-container'>
+                <Link className="navbar-logo" to="/">Receipe Blog</Link>
+                <div className='nav-menu'>
+                    <Link className='nav-links' to="/">Home</Link>
+                    <Link className='nav-links' to="/post">Post</Link>
+                    <Link className='nav-links' to="/contact">Contact Us</Link>
+                </div>
+            </div>
+            <Outlet />
         </div>
     )
 }
