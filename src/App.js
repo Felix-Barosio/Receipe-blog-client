5ebe42b64ea505ed1b7821a378f2f48a1346d222
Felix <barosiofelix@gmail.com>
2022-11-06 18:33:08 +0300

    Added api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react';
+import axios from "axios";
+import Post from './components/Post';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 
-
+const api = "http://localhost:9292/receipes"
 function App() {
 
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  const loadPosts = async () => {
+    const response = await axios.get(api);
+    setPost(response.data);
+  };
 
   return (
     <div className="container-fluid">
