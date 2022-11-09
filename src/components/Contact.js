7d6678eb74ae7efee8acb30f35e05f33dd590f21
Felix <barosiofelix@gmail.com>
2022-11-09 20:23:49 +0300

    Added information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,14 @@ function Contact() {
     return (
         <div>
             <div className='contact-us'>
+                <div className='contact-message'>
+                    <div className='message'>
+                        <h1>Contact Me.</h1>
+                        <p>Need to get in touch with us? <br />
+                            Fill in the inquiry form below.
+                        </p>
+                    </div>
+                </div>
                 <div className='contact-form'>
                     <ContactData />
                 </div>
